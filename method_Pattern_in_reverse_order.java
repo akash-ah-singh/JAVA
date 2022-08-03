cb06d2ed384e33f70f08fd7e6566207037cdacc3
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2022-08-03 13:06:13 +0530

    method Pattern in reverse order

---
+++ method_Pattern_in_reverse_order.java
@@ -0,0 +1,21 @@
+package Practice_Set_7;
+import java.util.Scanner;
+
+public class method_Pattern_in_reverse_order {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter any number to print reverse pattern : ");
+        int n=input.nextInt();
+
+        reverse_pattern(n);
+    }
+
+    static void reverse_pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=n;j>=i;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

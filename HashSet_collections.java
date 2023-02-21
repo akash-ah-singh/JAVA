1d4aa7f1732ec12a9c52915c0886f10afd6cc58c
Akash Singh <108462400+akash-ah-singh@users.noreply.github.com>
2023-02-21 14:39:58 +0530

    HashSet in collections

---
+++ HashSet_collections.java
@@ -0,0 +1,18 @@
+package Chap_15;
+import java.util.*;
+public class HashSet_collections {
+    public static void main(String[] args) {
+
+        HashSet<Integer> Myhash = new HashSet<>();
+        Myhash.add(12);
+        Myhash.add(18);
+        Myhash.add(16);
+        Myhash.add(2);
+        Myhash.add(9);
+
+        System.out.println(Myhash);
+
+        // there are Some methods of ArrayList, LinkedList and ArrayDeque which is also applied in HashSet  .
+
+    }
+}
